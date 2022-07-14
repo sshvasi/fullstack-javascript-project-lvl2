6071931634e759385c310d0764eeeffbc858dcbf
Alexander Vasiliev <asvasi25@gmail.com>
2022-07-14 14:03:24 +0300

    Define methods for format option and command arguments

--- gendiff.js
+++ gendiff.js
@@ -7,7 +7,9 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
 
