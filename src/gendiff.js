1cf1d8a1a2a86bf8a135f65d113e8dc97ef5eadb
Alexander Vasiliev <asvasi25@gmail.com>
2022-07-18 02:45:51 +0300

    feat: add test json files

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,5 +1,38 @@
-const gendiff = () => {
-  console.log('Gendiff is working!');
+import _ from 'lodash';
+
+const genDiff = (obj1, obj2) => {
+  const STATE_TYPES = {
+    added: '+',
+    deleted: '-',
+    unchanged: ' ',
+  };
+
+  const unitedKeys = _.union(Object.keys(obj1), Object.keys(obj2));
+  const sortedUnitedKeys = _.sortBy(unitedKeys);
+
+  const diff = sortedUnitedKeys.map((key) => {
+    const val1 = obj1[key];
+    const val2 = obj2[key];
+
+    if (!_.has(obj1, key)) {
+      return `  ${STATE_TYPES.added} ${key}: ${val2}`;
+    }
+
+    if (!_.has(obj2, key)) {
+      return `  ${STATE_TYPES.deleted} ${key}: ${val1}`;
+    }
+
+    if (val1 === val2) {
+      return `  ${STATE_TYPES.unchanged} ${key}: ${val1}`;
+    }
+
+    return [
+      `  ${STATE_TYPES.deleted} ${key}: ${val1}`,
+      `  ${STATE_TYPES.added} ${key}: ${val2}`,
+    ];
+  });
+
+  return diff;
 };
 
-export default gendiff;
+export default genDiff;
