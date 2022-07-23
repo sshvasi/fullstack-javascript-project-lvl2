4b9132263d3edac0df462831e18349c9837c8dfb
Alexander Vasiliev <asvasi25@gmail.com>
2022-07-23 22:27:07 +0300

    feat: add tests and fixtures for nested objects

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,16 +3,9 @@ import genDiff from '../index.js';
 
 const getFixturePath = (filename) => createPath(['__fixtures__', filename]);
 
-const expected = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+const expected = getFixturePath('expected.txt');
 
-describe('compare flat json files', () => {
+describe('compare nested json files', () => {
   test('positive scenario', () => {
     const filePath1 = getFixturePath('file1.json');
     const filePath2 = getFixturePath('file2.json');
@@ -23,7 +16,7 @@ describe('compare flat json files', () => {
   });
 });
 
-describe('compare flat yaml files', () => {
+describe('compare nested yaml files', () => {
   test('positive scenario', () => {
     const filePath1 = getFixturePath('file1.yml');
     const filePath2 = getFixturePath('file2.yml');
