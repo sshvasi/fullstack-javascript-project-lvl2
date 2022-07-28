8ee8a88535637e8e77a5c5bf315f2778733be038
Alexander Vasiliev <asvasi25@gmail.com>
2022-07-28 19:40:14 +0300

    fix: fix reading fixture file fn

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,15 @@
-import { createPath } from '../src/files.js';
+import { createPath, readFile } from '../src/files.js';
 import genDiff from '../index.js';
 
 const getFixturePath = (filename) => createPath(['__fixtures__', filename]);
 
-const expected = getFixturePath('expected.txt');
 
 describe('compare nested json files', () => {
   test('positive scenario', () => {
     const filePath1 = getFixturePath('file1.json');
     const filePath2 = getFixturePath('file2.json');
 
+    const expected = readFile(getFixturePath('expected.txt'));
     const received = genDiff(filePath1, filePath2);
 
     expect(received).toStrictEqual(expected);
@@ -21,6 +21,7 @@ describe('compare nested yaml files', () => {
     const filePath1 = getFixturePath('file1.yml');
     const filePath2 = getFixturePath('file2.yml');
 
+    const expected = readFile(getFixturePath('expected.txt'));
     const received = genDiff(filePath1, filePath2);
 
     expect(received).toStrictEqual(expected);
