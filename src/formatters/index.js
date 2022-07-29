754208707f54efe6b5ea78f8642795650ac6f1fc
Alexander Vasiliev <asvasi25@gmail.com>
2022-07-29 22:23:39 +0300

    feat: add plain formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,12 @@
 import stylish from './stylish.js';
+import plain from './plain.js';
 
 export default (diffTree, formatName) => {
   switch (formatName) {
     case 'stylish':
       return stylish(diffTree);
+    case 'plain':
+      return plain(diffTree);
     default:
       throw new Error(`Unexpected output format ${formatName}`);
   }
---
+++ src/formatters/plain.js
@@ -0,0 +1,45 @@
+import _ from 'lodash';
+import types from '../types.js';
+
+const formatNodeValue = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+
+  if (_.isString(value)) {
+    return `'${value}'`;
+  }
+
+  return value;
+};
+
+const formatNode = (node, path) => {
+  const { key, type, value, prevValue, children } = node;
+  const currentPath = [...path, key].join('.');
+
+  switch (type) {
+    case types.added:
+      return `Property '${currentPath}' was added with value: ${formatNodeValue(value)}`;
+
+    case types.deleted:
+      return `Property '${currentPath}' was removed`;
+
+    case types.changed:
+      return `Property '${currentPath}' was updated. From ${formatNodeValue(prevValue)} to ${formatNodeValue(value)}`;
+
+    case types.nested:
+      return children.flatMap((child) => formatNode(child, [...path, key]));
+
+    case types.unchanged:
+      return '';
+
+    default:
+      throw new Error(`Unexpected node type: ${type}.`);
+  }
+};
+
+const plainFormat = (diffTree) => diffTree
+  .flatMap((node) => formatNode(node, []))
+  .filter((node) => node);
+
+export default (diff) => plainFormat(diff).join('\n');
