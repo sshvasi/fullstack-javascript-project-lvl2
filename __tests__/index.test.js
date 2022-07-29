4f7cbb164e285efab4e380b9e3b8311cf487a847
Alexander Vasiliev <asvasi25@gmail.com>
2022-07-30 01:42:48 +0300

    feat: add tests for json formatter

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,27 +3,36 @@ import genDiff from '../index.js';
 
 const getFixturePath = (filename) => createPath(['__fixtures__', filename]);
 
-const formats = ['stylish', 'plain'];
-const extensions = ['json', 'yml'];
+const supportedOutputFormats = ['stylish', 'plain', 'json'];
+const supportedExtensions = ['json', 'yml'];
+
+const getCombinations = (formats, extensions) => {
+  const extCombinations = extensions
+    .flatMap((extension1) => extensions.map((extension2) => [extension1, extension2]));
+
+  return extCombinations
+    .flatMap((extComb) => formats.map((format) => [...extComb, format]));
+};
+
+const combinations = getCombinations(supportedOutputFormats, supportedExtensions);
+
 const expectedDiffs = new Map();
 
-const combinations = extensions.flatMap(
-  (extension) => formats.map((format) => [extension, format]),
-);
-
 beforeAll(() => {
-  formats.forEach((format) => {
+  supportedOutputFormats.forEach((format) => {
     const content = readFile(getFixturePath(`expected-${format}.txt`));
     expectedDiffs.set(format, content.trim());
   });
 });
 
-test.each(combinations)('diff .%s-file formatted as %s', (extension, format) => {
-  const filepath1 = getFixturePath(`file1.${extension}`);
-  const filepath2 = getFixturePath(`file2.${extension}`);
+test.each(combinations)(
+  '(.%s .%s)-files diff formatted as %s',
+  (extension1, extension2, format) => {
+    const filepath1 = getFixturePath(`file1.${extension1}`);
+    const filepath2 = getFixturePath(`file2.${extension2}`);
 
-  const actual = genDiff(filepath1, filepath2, format);
-  const expected = expectedDiffs.get(format);
+    const actual = genDiff(filepath1, filepath2, format);
+    const expected = expectedDiffs.get(format);
 
-  expect(actual).toStrictEqual(expected);
+    expect(actual).toStrictEqual(expected);
 });
